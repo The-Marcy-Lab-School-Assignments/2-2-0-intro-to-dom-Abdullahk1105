2da18f034dcca11c399de744828a2c03f0589e19
Abdullahk1105 <Abdullahk11051@gmail.com>
2024-05-14 16:24:03 -0400

    HW DONE WITH EDIT

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 
 // Question 9: Make it from scratch!
-
+const h1 = () =>{
 const mainHeading = document.createElement("h1");
     mainHeading.id = "main-heading";
     mainHeading.textContent = "Hello World!";
     document.body.appendChild(mainHeading);
+}
 
+const p = () => {
 const mainText = document.createElement("p");
     mainText.id = "main-text";
     mainText.className = "boring-text";
     mainText.textContent = "Look, I'm some text!";
     document.body.appendChild(mainText);
+}
+
+const main = () => {
+    h1();
+    p();
+
+}
+main();
